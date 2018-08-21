4c95ddeb0b20e02921c5d62c65aa256c43e65a4a
Dominque Cox <dcox@swifttrip.com>
2018-08-21 16:30:14 -0400

    Added Angular Forms

---
+++ app-prac/src/app/products.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html'
+})
+export class ProductsComponent {
+  productName = 'A Book';
+  isDisabled = true;
+  products = ['A book', 'A tree'];
+
+  constructor() {
+    setTimeout(() => {
+      this.productName = 'tree';
+      this.isDisabled = false;
+    },3000)
+  }
+
+  onAddProduct(form) {
+    // this.products.push(this.productName);
+    if (form.valid) {
+      this.products.push(form.value.productName);
+    }
+  }
+
+  onRemoveProduct(productName: string) {
+    this.products = this.products.filter(p => p !== productName);
+  }
+
+}
+
+
